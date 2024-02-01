e25e236f63b88249f5fe6fd1f5714607a20d506f
yumoxuan <2025543136@qq.com>
2024-02-01 14:37:50 +0800

    初始化项目

---
+++ src/test/java/com/yumoxuan/myCrm/MyCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumoxuan.myCrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
