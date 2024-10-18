21de61e8d42c6ae2ef7eacba84b05b3ec0afc997
zengjingchao <2025543136@qq.com>
2024-10-18 17:38:54 +0800

    feat:delete test module

--- src/main/java/com/yumoxuan/config/MybatisPlusConfig.java
+++ src/main/java/com/yumoxuan/config/MybatisPlusConfig.java
@@ -3,7 +3,7 @@ package com.yumoxuan.config;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
-@MapperScan("com.yumoxuan.myapp.*.mapper")
+@MapperScan("com.yumoxuan.*.mapper")
 @Configuration
 public class MybatisPlusConfig {
 
